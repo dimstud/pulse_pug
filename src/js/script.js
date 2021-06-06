b2f1d1719e371c7e07f9786a54f34ca9562cd0c0
Dmitry Ageev <dimichageev@gmail.com>
2021-06-06 15:14:19 +0300

    add toggle tabs

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,18 @@ $(document).ready(function() {
 			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
 			.closest('div.col-12').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 	});
-	
+
+	function toggleTabs(tab) {
+		$(tab).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			})
+		});
+	}
+
+	toggleTabs('.catalog-item__link');
+	toggleTabs('.catalog-item__back');
+
 });
